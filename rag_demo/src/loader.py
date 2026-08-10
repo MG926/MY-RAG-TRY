9d3740a90fa91b17561bdfa55b529c1a30589fb9
M.G <1617550506@qq.com>
2026-08-10 21:02:19 +0800

    完成了pdf文件的读取和分片的功能

--- rag_demo/src/loader.py
+++ rag_demo/src/loader.py
@@ -1,22 +1,28 @@
 import pdfplumber
 import pandas as pd
-from langchain_text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+""""该模块负责文件读取，目前仅支持pdf形式""""
 class loader:
     def read_pdf(self, pdf_path: str):
         """读取pdf文件"""
-        page_lists = []
+        #设置分割器的块大小为500，重叠大小为80
+        text_splitter = RecursiveCharacterTextSplitter(
+                chunk_size = 500,
+                chunk_overlap = 80
+        )
+        all_chunks = []
+        #利用pdfplumber读取pdf文字内容，并且分chunk存储在all_chunks中
         with pdfplumber.open(pdf_path) as pdf:
             for idx,page in enumerate(pdf.pages):
                 txt = page.extract_text() or ""
-                page_lists.append({
-                    "page_num": idx,
-                    "page_text": txt
-                })
-        text_splitter = RecursiveCharacterTextSplitter(
-            
-        )
-        return page_lists
-    
-    def read_doc(self, doc_path: str):
+                chunks = text_splitter.split_text(txt)
+                for chunk in chunks:
+                    all_chunks.append({
+                        "content": chunk,
+                        "metadata": {"page_num": idx}
+                    })
+        return all_chunks
+
 
 
