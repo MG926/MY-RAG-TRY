ae6fd614860202042f17eddac43a9586adcb2779
M.G <1617550506@qq.com>
2026-08-10 22:48:44 +0800

    修改了中文分词的句号，逗号和分号问题

--- rag_demo/src/loader.py
+++ rag_demo/src/loader.py
@@ -1,15 +1,16 @@
+#MG314
+"""该模块负责文件读取，目前仅支持pdf形式"""
 import pdfplumber
-import pandas as pd
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-"""该模块负责文件读取，目前仅支持pdf形式"""
-class loader:
+class Loader:
     def read_pdf(self, pdf_path: str):
         """读取pdf文件"""
         #设置分割器的块大小为500，重叠大小为80
         text_splitter = RecursiveCharacterTextSplitter(
                 chunk_size = 500,
-                chunk_overlap = 80
+                chunk_overlap = 80,
+                separators=["\n\n","\n","。",".",";","，",","," ",""]
         )
         all_chunks = []
         #利用pdfplumber读取pdf文字内容，并且分chunk存储在all_chunks中
@@ -20,7 +21,7 @@ class loader:
                 for chunk in chunks:
                     all_chunks.append({
                         "content": chunk,
-                        "metadata": {"page_num": idx}
+                        "metadata": {"page_num": idx+1}
                     })
         return all_chunks
 
