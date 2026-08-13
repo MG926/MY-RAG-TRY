68cb62c98e75589cadce1c6f3452cfb2f60d7d84
M.G <1617550506@qq.com>
2026-08-13 18:28:08 +0800

    优化了向量化操作的设备选择

--- rag_demo/src/embedding.py
+++ rag_demo/src/embedding.py
@@ -4,9 +4,11 @@ from sentence_transformers import SentenceTransformer
 import chromadb
 # import loader
 class Embedding:
-    def __init__(self, bge_path:str ="BAAI/bge-small-zh-v1.5", db_path:str="F:/my_RAG/rag_demo/chroma_db"):
+    def __init__(self, bge_path:str ="BAAI/bge-small-zh-v1.5", db_path:str="F:/my_RAG/rag_demo/chroma_db", device:str="cpu"):
+        #设置device
+        self.device = device
         #采用BGE方法，默认联网下载，如果有本地模型则上传至bge_path，默认采用cpu
-        self.bge_model = SentenceTransformer(bge_path,device="cpu")
+        self.bge_model = SentenceTransformer(bge_path,device=self.device)
         #本地化存储
         self.client = chromadb.PersistentClient(path=db_path)
     def bge_embedding(self, all_chunks:list[dict], collection_name:str = "default"):
@@ -35,11 +37,13 @@ class Embedding:
             documents= texts
         ) 
         print(f"内容数量:{len(texts)}")
-# my_loader = loader.Loader()
-# pdf_path = "F:/my_RAG/rag_demo/data/bianselong.pdf"
-# all_chunks = my_loader.read_pdf(pdf_path)
-# print(len(all_chunks))
-# bge_path = "F:/my_RAG/rag_demo/model/bge-small-zh-v1.5"
-# my_embedding = Embedding()
-# pdf_name = "bianselong"
-# my_embedding.bge_embedding(all_chunks=all_chunks,bge_path=bge_path,collection_name=pdf_name)
+'''测试代码'''
+# if __name__ == "__main__":
+#     my_loader = loader.Loader()
+#     pdf_path = "F:/my_RAG/rag_demo/data/bianselong.pdf"
+#     all_chunks = my_loader.read_pdf(pdf_path)
+#     print(len(all_chunks))
+#     bge_path = "F:/my_RAG/rag_demo/model/bge-small-zh-v1.5"
+#     my_embedding = Embedding()
+#     pdf_name = "bianselong"
+#     my_embedding.bge_embedding(all_chunks=all_chunks,bge_path=bge_path,collection_name=pdf_name)
