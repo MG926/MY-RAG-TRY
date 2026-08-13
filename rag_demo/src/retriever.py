7bb5d020f4ecfcb6422abd66a735f259fe0f7a92
M.G <1617550506@qq.com>
2026-08-13 18:29:56 +0800

    优化了设备选择

--- rag_demo/src/retriever.py
+++ rag_demo/src/retriever.py
@@ -5,9 +5,11 @@ import chromadb
 # import embedding
 # import loader
 class Retriever:
-    def __init__(self,bge_path:str ="BAAI/bge-small-zh-v1.5", db_path:str= "F:/my_RAG/rag_demo/chroma_db"):
+    def __init__(self,bge_path:str ="BAAI/bge-small-zh-v1.5", db_path:str= "F:/my_RAG/rag_demo/chroma_db", device:str="cpu"):
+        #设置device
+        self.device = device
         #开启BGE模型
-        self.recall_model = SentenceTransformer(bge_path,device="cpu")
+        self.recall_model = SentenceTransformer(bge_path,device=self.device)
         #开启向量库
         self.client = chromadb.PersistentClient(path=db_path)
     def recall(self, query:str, top_k:int =5, collection_name:str = "default"):
@@ -26,27 +28,29 @@ class Retriever:
             n_results=top_k
         )
         output = []
-        for filename,metadata,dist in zip(res["documents"][0],res["metadatas"][0],res["distances"][0]):
+        for texts,metadata,dist in zip(res["documents"][0],res["metadatas"][0],res["distances"][0]):
             output.append({
-                "content": filename,
+                "content": texts,
                 "metadata": metadata,
                 "distance": dist
             })
         return output
-# my_loader = loader.Loader()
-# pdf_path = "F:/my_RAG/rag_demo/data/bianselong.pdf"
-# all_chunks = my_loader.read_pdf(pdf_path)
-# print(len(all_chunks))
-# bge_path = "F:/my_RAG/rag_demo/model/bge-small-zh-v1.5"
-# my_embedding = embedding.Embedding()
-# pdf_name = "bianselong"
-# my_embedding.bge_embedding(all_chunks=all_chunks,bge_path=bge_path,collection_name=pdf_name)
-# my_retriever = Retriever()
-# query = "谁的手指被咬了？"
-# res = my_retriever.recall(query=query,top_k=5,bge_path=bge_path,collection_name=pdf_name)
-# for re in res:
-#     print("======================================================================")
-#     print("content:",re["content"])
-#     print("metadata:",re["metadata"])
-#     print("distance:",re["distance"])
+'''测试代码'''
+#if __name__ == "__main__":
+    # my_loader = loader.Loader()
+    # pdf_path = "F:/my_RAG/rag_demo/data/bianselong.pdf"
+    # all_chunks = my_loader.read_pdf(pdf_path)
+    # print(len(all_chunks))
+    # bge_path = "F:/my_RAG/rag_demo/model/bge-small-zh-v1.5"
+    # my_embedding = embedding.Embedding()
+    # pdf_name = "bianselong"
+    # my_embedding.bge_embedding(all_chunks=all_chunks,bge_path=bge_path,collection_name=pdf_name)
+    # my_retriever = Retriever()
+    # query = "谁的手指被咬了？"
+    # res = my_retriever.recall(query=query,top_k=5,bge_path=bge_path,collection_name=pdf_name)
+    # for re in res:
+    #     print("======================================================================")
+    #     print("content:",re["content"])
+    #     print("metadata:",re["metadata"])
+    #     print("distance:",re["distance"])
     
